42a984bd5397d7646a0224a7ba613ada23a928bf
zed jia <icy-i@163.com>
2021-09-18 11:17:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Accelerated & hosted by 4EVERLAND</h1>
+    
   </div>
 </template>
 
